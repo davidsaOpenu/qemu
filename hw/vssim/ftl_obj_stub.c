824e8d47817da0b5ba4d8b58dd7ba960398e3859
Shimi Gersner <gersner@gmail.com>
2018-07-05 20:33:25 +0000

    vssim: Implementes integration with VSSIM nvme simulator
    VSSIM allows realtime latency simulation on nvme devices.
    
    Implemtation consists of vssim block driver which passes
    all nvme requests through the VSSIM simulator. Currently
    vssim block drive only uses in-memory backing and does
    not support passthrough to child block devices.
    
    Block driver compilation is optional and uses standard
    configure flag --enable/disable-vssim.
    
    Drive configuration supports the following parameters
      size - In-memory disk size
      simulator - on/off flag to toggle simulator use
    
    Change-Id: I2f1bb1958456b26d56c049f214fe5282168cfdae
    Signed-off-by: Shimi Gersner <gersner@gmail.com>

---
+++ hw/vssim/ftl_obj_stub.c
@@ -0,0 +1,17 @@
+#include "simulator/common.h"
+#include "simulator/ftl_obj_strategy.h"
+
+void INIT_OBJ_STRATEGY(void)
+{
+
+}
+
+void TERM_OBJ_STRATEGY(void)
+{
+
+}
+
+ftl_ret_val _FTL_OBJ_COPYBACK(int32_t source, int32_t destination)
+{
+    return 0;
+}
---
+++ hw/vssim/vssim.c
@@ -0,0 +1,185 @@
+/* BlockDriver implementation for "vssim" format driver
+ *
+ * Copyright 2017 The Open University of Israel
+ *
+ * Author:
+ *   Shimi Gersner <gersner@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "qemu/osdep.h"
+#include "qemu/option.h"
+#include "qemu/cutils.h"
+#include "qemu/error-report.h"
+#include "qapi/qmp/qdict.h"
+#include "qapi/error.h"
+#include "block/block_int.h"
+#include "trace.h"
+
+#include "simulator/vssim_config_manager.h"
+#include "simulator/ftl_sect_strategy.h"
+
+#define VSSIM_FILE_EXTENSION                  (".vssim")
+#define VSSIM_FILE_EXTENSION_CHARACTER_LENGTH (sizeof(VSSIM_FILE_EXTENSION)-1)
+
+#define VSSIM_DEFAULT_FILE_SIZE_IN_BYTES      (1024*1024*256)
+
+#define VSSIM_BLOCK_OPT_SIMULATOR             ("simulator")
+
+static bool g_device_open = false;
+
+typedef struct BDRVVSSIMState {
+    char * memory;
+    uint64_t size;
+    bool simulator;
+} BDRVVSSIMState;
+
+static QemuOptsList runtime_opts = {
+    .name = "vssim",
+    .head = QTAILQ_HEAD_INITIALIZER(runtime_opts.head),
+    .desc = {
+        {
+            .name = BLOCK_OPT_SIZE,
+            .type = QEMU_OPT_SIZE,
+            .help = "disk size",
+        },
+        {
+            .name = VSSIM_BLOCK_OPT_SIMULATOR,
+            .type = QEMU_OPT_BOOL,
+            .help = "enable simulator"
+        },
+        { /* end of list */ }
+    },
+};
+
+static int vssim_open(BlockDriverState *bs, QDict * dict, int flags,
+                      Error **errp)
+{
+    BDRVVSSIMState *s = bs->opaque;
+    QemuOpts *opts = NULL;
+    trace_vssim_open(bs);
+
+    // Only a single device is allowed due to global use of FTL
+    if (g_device_open) {
+        error_setg(errp, "vssim device allows only a single instance");
+        return -EINVAL;
+    }
+    g_device_open = true;
+
+    // Prase the drive options
+    opts = qemu_opts_create(&runtime_opts, NULL, 0, &error_abort);
+    qemu_opts_absorb_qdict(opts, dict, &error_abort);
+    s->size = qemu_opt_get_size(opts, BLOCK_OPT_SIZE,
+                                VSSIM_DEFAULT_FILE_SIZE_IN_BYTES);
+    s->simulator = qemu_opt_get_bool(opts, VSSIM_BLOCK_OPT_SIMULATOR, true);
+    qemu_opts_del(opts);
+
+    // Allocate the memory
+    s->memory = qemu_blockalign0(bs, s->size);
+
+    // Initialize FTL and logger
+    if (s->simulator) {
+        FTL_INIT();
+        INIT_LOG_MANAGER();
+    }
+
+    trace_vssim_initialized(bs, s->size, s->memory);
+
+    return 0;
+}
+
+static void vssim_close(BlockDriverState *bs)
+{
+    BDRVVSSIMState *s = bs->opaque;
+    trace_vssim_close(bs);
+
+    // Destruct FTL
+    if (s->simulator) {
+        TERM_LOG_MANAGER();
+        FTL_TERM();
+    }
+
+    // Clear memory
+    if (NULL != s->memory) {
+        qemu_vfree(s->memory);
+    }
+
+    // Clear singleton state
+    g_device_open = false;
+}
+
+static int coroutine_fn vssim_co_preadv(BlockDriverState *bs, uint64_t offset,
+        uint64_t bytes, QEMUIOVector *qiov, int flags)
+{
+    BDRVVSSIMState *s = bs->opaque;
+    trace_vssim_read(bs, offset, bytes);
+
+    // Read from memory
+    qemu_iovec_from_buf(qiov, 0, s->memory + offset, bytes);
+
+    // Pass write to simulator
+    if (s->simulator) {
+        _FTL_READ_SECT(offset / GET_SECTOR_SIZE(), bytes/GET_SECTOR_SIZE());
+    }
+
+    return 0;
+}
+
+static int coroutine_fn vssim_co_pwritev(BlockDriverState *bs, uint64_t offset,
+        uint64_t bytes, QEMUIOVector *qiov, int flags)
+{
+    BDRVVSSIMState *s = bs->opaque;
+    trace_vssim_write(bs, offset, bytes);
+
+    // Write to iovec buffer
+    qemu_iovec_to_buf(qiov, 0, s->memory + offset, bytes);
+
+    // Pass write to simulator
+    if (s->simulator) {
+        _FTL_WRITE_SECT(offset / GET_SECTOR_SIZE(), bytes/GET_SECTOR_SIZE());
+    }
+
+    return 0;
+}
+
+static int64_t vssim_getlength(BlockDriverState *bs)
+{
+    BDRVVSSIMState *s = bs->opaque;
+    return s->size;
+}
+
+BlockDriver bdrv_vssim = {
+    .format_name            = "vssim",
+    .instance_size          = sizeof(BDRVVSSIMState),
+    .bdrv_open              = vssim_open,
+    .bdrv_co_preadv         = vssim_co_preadv,
+    .bdrv_co_pwritev        = vssim_co_pwritev,
+    .bdrv_close             = vssim_close,
+    .bdrv_getlength         = vssim_getlength,
+    .bdrv_needs_filename    = false,
+    .has_variable_length    = false
+};
+
+static void bdrv_vssim_init(void)
+{
+    bdrv_register(&bdrv_vssim);
+}
+
+block_init(bdrv_vssim_init);
